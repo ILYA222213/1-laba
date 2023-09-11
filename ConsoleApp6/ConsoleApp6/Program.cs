4032eb146cb33b176682fd544f531c2041045cdd
Сом <117897751+ILYA222213@users.noreply.github.com>
2023-09-11 12:09:32 +0300

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Multiple(double x, double y)
+    {
+        if (x == 0 | y == 0)
+            Console.WriteLine("Result: 0");
+        else Console.WriteLine("Result: " + x / (1 / y));
+    }
+
+
+    static void Main()
+    {
+        double x = double.Parse(Console.ReadLine());
+        double y = double.Parse(Console.ReadLine());
+        Multiple(x, y);
+
+    }
+}
